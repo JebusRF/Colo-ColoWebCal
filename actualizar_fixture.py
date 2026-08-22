fdb6d2f705a74e7ba902a0e5b8c315fa85f1b879
JebusRF <jebusramirez42@gmail.com>
2026-08-22 15:08:26 -0400

    Add functions to fetch and create calendar events

--- actualizar_fixture.py
+++ actualizar_fixture.py
@@ -1,3 +1,12 @@
+from icalendar import Calendar, Event, Alarm
+from datetime import datetime, timedelta
+import requests
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+
 def obtener_eventos():
 
     eventos = []
@@ -32,3 +41,58 @@ def obtener_eventos():
         pagina += 1
 
     return eventos
+
+
+def obtener_detalle_evento(url):
+
+    url = url.replace("http://", "https://")
+
+    r = requests.get(
+        url,
+        headers=HEADERS,
+        timeout=30
+    )
+
+    r.raise_for_status()
+
+    return r.json()
+
+
+def crear_calendario():
+
+    cal = Calendar()
+
+    cal.add(
+        "prodid",
+        "-//JebusRF Colo-Colo WebCal//"
+    )
+
+    cal.add("version", "2.0")
+
+    refs = obtener_eventos()
+
+    print(f"EVENTOS ENCONTRADOS: {len(refs)}")
+
+    total = 0
+
+    for ref in refs:
+
+        try:
+
+            partido = obtener_detalle_evento(ref)
+
+            fecha = partido["date"]
+            titulo = partido["name"]
+            uid = f"{partido['id']}@jebusrf"
+
+            inicio = datetime.fromisoformat(
+                fecha.replace("Z", "+00:00")
+            )
+
+            termino = inicio + timedelta(hours=2)
+
+            estadio = "Por confirmar"
+
+            try:
+                estadio = (
+                    partido["
