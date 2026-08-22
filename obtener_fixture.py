1d3acf0eee4128b1933cd34a39fb7c55dea630a6
JebusRF <jebusramirez42@gmail.com>
2026-08-22 14:18:58 -0400

    Update URL and variable name in obtener_fixture.py

--- obtener_fixture.py
+++ obtener_fixture.py
@@ -1,18 +1,13 @@
 import requests
 import json
 
-URL = "https://sports.core.api.espn.com/v2/sports/soccer/leagues/chi.1/events/401850418"
+URL = "https://sports.core.api.espn.com/v2/sports/soccer/leagues/chi.1/events/401850418/competitions/401850418/competitors/2688"
 
-respuesta = requests.get(
+r = requests.get(
     URL,
-    headers={
-        "User-Agent": "Mozilla/5.0"
-    },
+    headers={"User-Agent": "Mozilla/5.0"},
     timeout=30
 )
 
-print("STATUS:", respuesta.status_code)
-
-print("\n=== EVENTO ===\n")
-
-print(json.dumps(respuesta.json(), indent=2))
+print("STATUS:", r.status_code)
+print(json.dumps(r.json(), indent=2))
