97a879756543dfc28f4bc3db4dd18520014461a4
JebusRF <jebusramirez42@gmail.com>
2026-08-22 10:04:40 -0400

    Update actualizar_fixture.py

--- actualizar_fixture.py
+++ actualizar_fixture.py
@@ -1,5 +1,6 @@
 from icalendar import Calendar, Event
 from datetime import datetime, timedelta
+
 PARTIDOS = [
     (
         "2026-08-23 15:00",
@@ -74,21 +75,35 @@ PARTIDOS = [
         "Primera División"
     )
 ]
+
 cal = Calendar()
 cal.add("prodid", "-//JebusRF Colo-Colo WebCal//")
 cal.add("version", "2.0")
+
 for i, (fecha, titulo, estadio, torneo) in enumerate(PARTIDOS):
+
     inicio = datetime.strptime(fecha, "%Y-%m-%d %H:%M")
     termino = inicio + timedelta(hours=2)
+
     evento = Event()
+
     # UID único para que Outlook actualice en lugar de duplicar
     evento.add("uid", f"colocolo-2026-{i}@jebusrf")
+
     evento.add("summary", titulo)
     evento.add("location", estadio)
+
     evento.add(
         "description",
         f"Torneo: {torneo}\\nEstadio: {estadio}"
     )
+
     evento.add("dtstart", inicio)
     evento.add("dtend", termino)
-    cal.add_component
+
+    cal.add_component(evento)
+
+with open("docs/colocolo.ics", "wb") as archivo:
+    archivo.write(cal.to_ical())
+
+print("CALENDARIO GENERADO CORRECTAMENTE")
