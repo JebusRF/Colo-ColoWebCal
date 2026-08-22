7f9768df191e6c278aae197dc6f0c10a8c13e9cc
JebusRF <jebusramirez42@gmail.com>
2026-08-22 14:08:27 -0400

    Update URL and print statements for event details

--- obtener_fixture.py
+++ obtener_fixture.py
@@ -1,16 +1,18 @@
 import requests
 import json
 
-URL = "https://sports.core.api.espn.com/v2/sports/soccer/leagues/chi.1/seasons/2026/teams/2688/events"
+URL = "https://sports.core.api.espn.com/v2/sports/soccer/leagues/chi.1/events/401850418"
 
 respuesta = requests.get(
     URL,
-    headers={"User-Agent": "Mozilla/5.0"},
+    headers={
+        "User-Agent": "Mozilla/5.0"
+    },
     timeout=30
 )
 
 print("STATUS:", respuesta.status_code)
 
-print("\n=== EVENTOS COLO-COLO ===\n")
+print("\n=== EVENTO ===\n")
 
 print(json.dumps(respuesta.json(), indent=2))
