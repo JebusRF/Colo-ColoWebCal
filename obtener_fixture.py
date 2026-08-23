bbfbf04a32f7a80aa49863a7a15c106f52ea5278
JebusRF <jebusramirez42@gmail.com>
2026-08-23 11:12:50 -0400

    Change API URL and enhance JSON response handling
    Updated the API URL and improved error handling for JSON response.

--- obtener_fixture.py
+++ obtener_fixture.py
@@ -1,19 +1,19 @@
 import requests
+import json
 
-url = "https://sports.core.api.espn.com/v2/sports/soccer/leagues"
+url = "https://sports.core.api.espn.com/v2/sports/soccer/leagues/conmebol.sudamericana"
 
 r = requests.get(
     url,
-    headers={"User-Agent": "Mozilla/5.0"},
+    headers={
+        "User-Agent": "Mozilla/5.0"
+    },
     timeout=30
 )
 
-data = r.json()
-
 print("STATUS:", r.status_code)
 
-for item in data["items"]:
-    ref = item["$ref"]
-
-    if "/chi" in ref.lower():
-        print(ref)
+try:
+    print(json.dumps(r.json(), indent=2))
+except Exception:
+    print(r.text)
